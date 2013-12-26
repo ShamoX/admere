aa6312b90b95d03ba1090ac69c38d2d627136502
Roland Laurès <roland.laures@gnuside.com>
2013-12-26 14:07:31 -0300

    update to new implementation of Admere

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
 cd test
-ocamlopt.opt -I /Users/roland/ocamlbrew/ocaml-4.00.1/.opam/system/lib/batteries -I ../_build/src/ unix.cmxa graphics.cmxa str.cmxa nums.cmxa bigarray.cmxa batteries.cmxa AdmereExceptions.cmx AdmereUtils.cmx libAdmere.cmxa testExample.ml testAdmereDisplayer.ml -o testAdmereDisplayer && rm -fr *.cm* *.o && ./testAdmereDisplayer
+ocamlopt.opt -I /Users/roland/ocamlbrew/ocaml-4.00.1/.opam/system/lib/batteries -I ../_build/src/ unix.cmxa graphics.cmxa str.cmxa nums.cmxa bigarray.cmxa batteries.cmxa AdmereExceptions.cmx AdmereUtils.cmx libAdmere.cmxa testExample.ml -o testExample && rm -fr *.cm* *.o && ./testExample
 cd -
--- test/testExample.ml
+++ test/testExample.ml
@@ -3,7 +3,7 @@ open AdmereImplementation
 open AdmereParameter
 open Admere;;
 
-Random.init 10 ;;
+Random.self_init () ;;
 
 module TestParameter =
   struct
@@ -16,19 +16,37 @@ module TestParameter =
 module TestImplementation =
   struct
     type t = float
-    let maxValue = 5.0;;
+    let maxValue = 200000.;;
 
     let mayAdjustRefinement v =
-      if v > 4.0 then DoRefine
-      else if v > 1.0 then DontRefine
+      if v > 2000.0 then DoRefine
+      else if v > 500.0 then DontRefine
       else DoUnrefine
     and getInitialData coor =
-      Random.float maxValue
+      (Random.float maxValue)
     and getColor v =
-      Graphics.rgb 135 135 (int_of_float (255. *. v /. 5.0))
+      let rgb = ((2. ** 24.) *. v) /. maxValue in (* put v between 0 and 2^24 *)
+      let r = (int_of_float (rgb /. (2. ** 16.)))
+      and g = (int_of_float (mod_float (rgb /. (2. ** 8.)) ((2. ** 16.) -. 1.)))
+      and b = (int_of_float (mod_float (rgb /. (2. ** 0.)) ((2. ** 8.) -. 1.)))
+      in Graphics.rgb r g b
     and toStr v = Printf.sprintf "%f" v
+    and doRefine level v oldCoor newCoor =
+      v /. (2. ** (float_of_int TestParameter.dim))
+    ;;
+    let rec doUnrefine level newCoor = function
+      [] -> 0.0
+      | (v,oldCoor)::l -> v +. (doUnrefine level newCoor l)
     ;;
 
   end
 
-module Test = Admere(TestImplementation)(TestParameter)
+module Test = Admere(TestImplementation)(TestParameter);;
+
+Test.drawGrid ~displayGrid:true ();
+Sys.command "sleep 3";
+while Test.checkRefinement () do
+  Test.drawGrid ~displayGrid:true ();
+  Sys.command "sleep 3"
+done;
+Sys.command "sleep 30"
