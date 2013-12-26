2c025f15321d75d29fb01b87e62bd509289ac653
Roland Laurès <roland.laures@gnuside.com>
2013-12-26 01:47:21 -0300

    added refinement and merging function needed for AMR algorythm

--- src/admereImplementation.mli
+++ src/admereImplementation.mli
@@ -16,6 +16,31 @@ module type AdmereImplementation =
      * This means that your module may have a normalisation function and an
      * unormalisation function to be able to access correctly to the cells. *)
 
+    (** Function called when refinement is apply to a cell *)
+    val doRefine : int -> t -> float array -> t
+    (** Here are a description of the arguments passed to this function :
+      * - The first argument given is the new cell level (if the splitted cell
+      * was level l, this argument is l+1.
+      * - Second argument is the content of the parent cell (this will be erased
+      * whence the new subCells are made.
+      * - Third argument is the relative position of the new cell regarding the
+      * center of the refined cell.
+      *
+      * -> returned value will be use as the cell content for the new cell.
+      * *)
+
+    (** Function called when unrefinement is apply to a group of cells *)
+    val doUnrefine : int -> (t * float array) array -> t
+    (** Here are a description of the arguments passed to this function :
+      * - The first is the new cell level (if the merging cells were level l,
+      * this argument will be l-1).
+      * - Second argument is an array of couple cell content + relative position
+      * regarding the center of the futur cell.
+      *
+      * -> returned value will be use as the cell content of the merge
+      * cell
+      * *)
+
     (** This function allow to use with AdmereDisplay module *)
     val getColor : t -> Graphics.color
     (** It takes a value and convert it to a colored value to be displayed *)
